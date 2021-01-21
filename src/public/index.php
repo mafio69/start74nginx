95abc1900ce425730b8bb2256a41211f6abcfbde
mafio69 <mf1969@com>
2021-01-21 10:34:55 +0100

    Final

--- src/public/index.php
+++ src/public/index.php
@@ -1,13 +1,13 @@
 <?php
 include '../vendor/autoload.php';
+
 /** @var RouteCollector $collector */
 
 use Phroute\Phroute\RouteCollector;
 use Phroute\Phroute\Dispatcher;
 
-
 require("../App/Routes/routes.php");
-$dispatcher =  new Dispatcher($collector->getData());
+$dispatcher = new Dispatcher($collector->getData());
 $response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
 echo $response;
